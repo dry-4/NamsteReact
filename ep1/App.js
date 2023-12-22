e2c3058f7a0fd2709b4b9f65755ca2a8bbaf759f
Sudhanshu Singh <32094895+Sudhanshu0710@users.noreply.github.com>
2023-12-22 09:50:22 +0530

    Nmaste React ep01 part04 parent, child & siblings

--- ep1/App.js
+++ ep1/App.js
@@ -1,5 +1,32 @@
-const heading = React.createElement('h1', {id : "heading"}, 'Namste React!');
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+// Nested HTML Components in ReactJS
+const parent = React.createElement(
+   'div', {id : "parent"}, 
+   React.createElement('div', {id : "child"},
+      React.createElement('div', {id : "grandchild"},
+         React.createElement('h1', {id : "heading"}, 'Namste React from heading tag!')
+      )
+   )
+);
 
-console.log(heading);
+// for more than 1 child element, we can use array of elements as well
+const antotherParent = React.createElement(
+   'div', {id : "parent"}, 
+   React.createElement('div', {id : "child"},
+      [  React.createElement('div', {id : "grandchild1"},
+         [  React.createElement('h1', {id : "heading"}, 'Namste React from heading tag from grandchild1!'),
+            React.createElement('h2', {id : "subheading"}, 'Namste React from subheading tag from grandchild1!')]
+         ),
+         React.createElement('div', {id : "grandchild2"},
+         [  React.createElement('h1', {id : "heading"}, 'Namste React from heading tag from grandchild2!'),
+            React.createElement('h2', {id : "subheading"}, 'Namste React from subheading tag from grandchild2!')]
+         )
+      ]
+   )
+);
+
+// Above code looks very complex and hard to read. So, we can use JSX to make it more readable.
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(antotherParent);
+
+console.log(antotherParent);
